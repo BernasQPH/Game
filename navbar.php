da75ee2cbc579f0222128a4b1f023c017aa93f66
Bernardo Henriques <bernardo.henriques02@gmail.com>
2024-12-24 19:04:02 +0000

    nav

---
+++ navbar.php
@@ -0,0 +1,36 @@
+<?php
+include 'config.php';
+
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+
+
+?>
+<nav class="navbar navbar-expand-lg  bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="index.php"><i class="bi bi-controller"></i> GAME</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item"><a class="nav-link" href="../aboutus.php">Sobre Nós</a></li>
+                <li class="nav-item"><a class="nav-link" href="../Contactus.php">Contactos</a></li>
+                <li class="nav-item"><a class="nav-link" href="../search.php">Produtos</a></li>
+            </ul>
+            <ul class="navbar-nav">
+                <?php if (isset($_SESSION['user_id'])): ?>
+                    
+                    <a class="nav-link">Bem-vindo, <?php echo htmlspecialchars($_SESSION['user_name']); ?></a>
+                    <li class="nav-item"><a class="nav-link" href="../logout.php">Logout</a></li>
+                <?php else: ?>
+                  
+                    <li class="nav-item"><a class="nav-link" href="registo.php">Registar</a></li>
+                    <li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>
+                <?php endif; ?>
+            </ul>
+        </div>
+    </div>
+</nav>
